6768f3c0c52ba3035a7e50625c24097b0ea1eae8
evanmc21 <emcandler21@gmail.com>
2018-11-10 08:41:00 -0500

    a user can now login

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,7 +1,40 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+
+
+const Auth = require('../../models/Auth');
 
 router.get('/test', (req, res) => res.json({ msg: 'Auth Works' }));
 
+router.post('/signup', (req, res) => {
+  Auth.findOne({ email: req.body.email }).then(
+    auth => {
+       if(auth) {res.status(400).json({ email: 'email already exists'})
+    } else {
+      const avatar = gravatar.url('req.body.email', {
+        s:'200',
+        r: 'pg',
+        d: 'mm'
+      });
+
+      const newAuth = new Auth({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        avatar
+      });
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newAuth.password, salt, (err, hash) => {
+          if(err) throw err;
+          newAuth.password = hash;
+          newAuth.save().then(auth => res.json(auth))
+          .catch(err => console.log(err));
+        }); 
+      });
+    }
+  });
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const bodyParser = require('body-parser');
 const auth = require('./routes/api/auth');
 const profiles = require('./routes/api/profiles');
 const posts = require('./routes/api/posts');
 
 const app = express();
 
+
+// middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// database
 const db = require('./config/keys').mongoURI;
 
 mongoose.connect(db).then(() => console.log('DB connected!'))
