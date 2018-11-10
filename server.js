12a67b5113327e0935d80254b241b7b4ab10f1bb
evanmc21 <emcandler21@gmail.com>
2018-11-09 23:49:10 -0500

    established database connection

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+const db = require('./config/keys').mongoURI;
+
+mongoose.connect(db).then(() => console.log('DB connected!'))
+.catch(err => console.log(err));
 
 app.get('/', (req, res) => res.send('Haaayy'));
 
