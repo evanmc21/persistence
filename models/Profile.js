ae0e50cc299e741e84fa3d39edc042a62dc00fb6
evanmc21 <emcandler21@gmail.com>
2018-11-12 12:05:27 -0500

    definted profile schema

--- models/Profile.js
+++ models/Profile.js
@@ -5,6 +5,114 @@ const ProfileSchema = new Schema({
   user: {
     // associates user by the ID
     type: Schema.Types.ObjectId,
-    ref: ''
+    ref: 'users'
+  },
+  handle: {
+    type: String,
+    required: true,
+    max: 25
+  },
+  bio: {
+    type: String,
+    required: true
+  },
+  website: {
+    type: String
+  },
+  specialty: {
+    type: String
+  },
+  location: {
+    type: String
+  },
+  experienceLevel: {
+    type: String,
+    required: true
+  },
+  education: [
+    {
+      school: {
+        type: String,
+        required: true
+      },
+      degree: {
+        type: String,
+        required: true
+      },
+      fieldofStudy: {
+        type: string,
+        required: true
+      },
+      from: {
+        type: Date,
+        required: true
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: {
+        type: String
+      }
+    }
+  ],
+  favoriteQuote: {
+    type: String
+  },
+  skills: {
+    type: [string],
+    required: true
+  },
+  experience: [
+    {
+      title: {
+        type: String,
+        required: true
+      },
+      company: {
+        type: String,
+        required: true
+      },
+      location: {
+        type: string
+      },
+      from: {
+        type: Date,
+        required: true
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: {
+        type: String
+      }
+    }
+  ],
+  social: {
+    youtube: {
+      type: String
+    },
+    instagram: {
+      type: String
+    },
+    linkedin: {
+      type: String
+    },
+    twitter: {
+      type: String
+    }
+  },
+  date: {
+    type: Date,
+    default: Date.now
   }
 });
+
+module.exports = Profile = mongoose.model('profile', ProfileSchema);
