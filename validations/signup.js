7918fdae0b3efa7186a8bea70b152051863c15df
evanmc21 <emcandler21@gmail.com>
2018-11-15 15:08:41 -0500

    can register a user from the frontend

--- validations/signup.js
+++ validations/signup.js
@@ -15,18 +15,20 @@ const validateSignUp = data => {
   if (validator.isEmpty(data.name)) {
     errors.name = "don't be coy. tell us your name.";
   }
-  if (validator.isEmpty(data.email)) {
-    errors.email = 'please enter an email';
-  }
   if (!validator.isEmail(data.email)) {
     errors.email = 'email is invalid';
   }
-  if (validator.isEmpty(data.password)) {
-    errors.password = 'please enter a password.';
+  if (validator.isEmpty(data.email)) {
+    errors.email = 'please enter an email';
   }
   if (!validator.isLength(data.password, { min: 8, max: 20 })) {
     errors.password = 'password is invalid.';
   }
+
+  if (validator.isEmpty(data.password)) {
+    errors.password = 'please enter a password.';
+  }
+
   if (validator.isEmpty(data.password2)) {
     errors.password2 = 'please confirm your password';
   }
