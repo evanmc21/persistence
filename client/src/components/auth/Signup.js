f8d6a1e8c399c394620f5e3affeb198f6e3c46aa
evanmc21 <emcandler21@gmail.com>
2018-11-19 11:34:44 -0500

    updated email schema to lowercase

--- client/src/components/auth/Signup.js
+++ client/src/components/auth/Signup.js
@@ -93,21 +93,6 @@ class Signup extends Component {
                   onChange={this.onChange}
                   error={errors.password2}
                 />
-                <div className="form-group">
-                  <input
-                    type="password"
-                    className={classnames('form-control form-control-lg', {
-                      'is-invalid': errors.password2
-                    })}
-                    placeholder="Confirm Password"
-                    name="password2"
-                    value={this.state.password2}
-                    onChange={this.onChange}
-                  />
-                  {errors.password2 && (
-                    <div className="invalid-feedback">{errors.password2}</div>
-                  )}
-                </div>
                 <input type="submit" className="btn btn-info btn-block mt-4" />
               </form>
             </div>
