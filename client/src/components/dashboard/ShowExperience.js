fdff78da3031477d6df13a7d6d2e83d9a0426fed
evanmc21 <emcandler21@gmail.com>
2018-11-20 14:39:33 -0500

    can delete experience from dashboard

--- client/src/components/dashboard/ShowExperience.js
+++ client/src/components/dashboard/ShowExperience.js
@@ -2,8 +2,12 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Moment from 'react-moment';
+import { removeExperience } from '../../actions/profile';
 
 class ShowExperience extends Component {
+  onDeleteClick = id => {
+    this.props.removeExperience(id);
+  };
   render() {
     const experience = this.props.experience.map(exp => (
       <tr key={exp._id}>
@@ -18,25 +22,40 @@ class ShowExperience extends Component {
           )}
         </td>
         <td>
-          <button className="btn btn-danger active">Remove Experience</button>
+          <button
+            onClick={this.onDeleteClick.bind(this, exp._id)}
+            className="btn btn-sm btn-danger active"
+          >
+            Remove Experience
+          </button>
         </td>
       </tr>
     ));
 
     return (
       <div>
-        <h3 class="mb-4">Experience</h3>
+        <h3 className="mb-4">Experience</h3>
         <table className="table">
-          <tr>
-            <th>Company</th>
-            <th>Title</th>
-            <th>Years</th>
-          </tr>
-          {experience}
+          <thead>
+            <tr>
+              <th>Company</th>
+              <th>Title</th>
+              <th>Years</th>
+              <th />
+            </tr>
+            {experience}
+          </thead>
         </table>
       </div>
     );
   }
 }
 
-export default connect(null)(ShowExperience);
+ShowExperience.propTypes = {
+  removeExperience: PropTypes.func.isRequired
+};
+
+export default connect(
+  null,
+  { removeExperience }
+)(ShowExperience);
