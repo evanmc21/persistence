9987e8bbc16aa8c47b9f2cc0418ae0fc929a001d
evanmc21 <emcandler21@gmail.com>
2018-11-21 15:11:01 -0500

    set up production keys

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoURI:
-    "mongodb://evan:beyonce21@ds157853.mlab.com:57853/persistence",
-  secretOrKey: 'flawless'
-};
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
+}
